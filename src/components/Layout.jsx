ac5e33dce8d7df82df07df531f00e2a3c7b09222
Drish <drish.xd@gmail.com>
2022-01-06 00:00:47 +0530

    added fixed layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+import Contact from "./Contacts/Contact";
+import PropTypes from "prop-types";
+
+const Layout = ({children}) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Contact />
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
