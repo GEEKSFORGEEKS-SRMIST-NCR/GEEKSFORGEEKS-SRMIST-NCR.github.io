49c2697f25d038a6e83d38641ac375c005a66619
Drish <drish.xd@gmail.com>
2022-01-06 00:28:17 +0530

    fix 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+// styles
+const pageStyles = {
+  color: "var(--text-main)",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "calc(100vh - 313px)",
+  textAlign: "center",
+};
+
+const paragraphStyles = {
+  fontSize: "1.25rem",
+  marginBottom: 10,
+};
+
+const homelink = {
+  color: "var(--text-main)",
+  transition: "all 0.2s ease-in-out",
+  "&:hover": {
+    color: "#2c3e50",
+    borderBottom: "1px solid #2c3e50",
+  },
+};
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <main style={pageStyles}>
+        <h1 className="section-title">Page not found</h1>
+        <p style={paragraphStyles}>
+          Sorry 😔 we couldn’t find what you were looking for.
+          <br />
+          <br />
+          <Link to="/" style={homelink}>
+            Go home
+          </Link>
+        </p>
+      </main>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
