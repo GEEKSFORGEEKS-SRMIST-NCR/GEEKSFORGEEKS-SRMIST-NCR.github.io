649ff1eeebb932f067f7ded968199475251fee2f
Drish <drish.xd@gmail.com>
2022-01-05 13:37:31 +0530

    Homepage Update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Home.css'
+import { Logo2 } from "../Logo/Logo2"
 
 const Home = () => {
     return (
-        <div className='home'>
-            Home Page
-        </div>
+        <section className='home'>
+            <Logo2 />
+            <h1>SRMIST NCR CHAPTER</h1>
+            <h2>A Community of SRMIST Students</h2>
+        </section>
     )
 }
 
