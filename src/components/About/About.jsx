af0a934c92a7ab2ad1ead4d3618fcb382c88a431
Drish <drish.xd@gmail.com>
2022-01-05 14:15:10 +0530

    added about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
+import { Logo } from "../Logo/Logo";
 
 const About = () => {
-    return (
-        <div id='about' className='about'>
-            About Page
-        </div>
-    )
-}
+  return (
+    <section id="about" className="about">
+      <div className="about-container">
+        <h1> <Logo /> About Us</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo totam nisi beatae iure natus fuga, autem ab quia reprehenderit commodi vel assumenda officiis laboriosam.
+          Maxime, sint voluptatem! Consequatur reiciendis voluptatibus dolore doloremque esse! Corporis, voluptas sit tenetur eos quas aliquid autem veritatis recusandae nisi
+          dolore. Totam ratione iste numquam dolorem.
+          <br />
+          &emsp; Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, aliquam?
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
