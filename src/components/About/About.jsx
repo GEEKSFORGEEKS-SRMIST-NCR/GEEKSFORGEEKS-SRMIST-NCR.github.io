2c8a71fec497f91f5d2f2cbcc039badd89f6c82e
Drish <drish.xd@gmail.com>
2022-01-05 19:41:54 +0530

    added about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./About.css";
 import { Logo } from "../Logo/Logo";
+import { Logo2 } from "../Logo/Logo2";
 
 const About = () => {
   return (
     <section id="about" className="about">
+      <h1 className="section-title"> About Us</h1>
       <div className="about-container">
-        <h1> <Logo /> About Us</h1>
+        <div className="title">
+          <Logo className="logo" /> <Logo2 />
+        </div>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo totam nisi beatae iure natus fuga, autem ab quia reprehenderit commodi vel assumenda officiis laboriosam.
           Maxime, sint voluptatem! Consequatur reiciendis voluptatibus dolore doloremque esse! Corporis, voluptas sit tenetur eos quas aliquid autem veritatis recusandae nisi
