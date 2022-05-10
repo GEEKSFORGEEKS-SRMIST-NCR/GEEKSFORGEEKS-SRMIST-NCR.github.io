c8351000c5ae22213fc69058a365ae2b4ad0a95e
Drish-xD <drish.xd@gmail.com>
2022-05-10 23:21:42 +0530

    update events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -11,16 +11,16 @@ let events = [
       "This resume building workshop will help you build a polished resume that could land you an interview.🥳",
   },
   {
-    id: 1,
-    title: "Event Title 2",
+    id: 2,
+    title: "KODATION",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
+      "An amazing technical Contest specially only for the SRMIST Delhi NCR students, conducted on it's very own trusted platform of GeeksforGeeks, this contest was divided into two groups.",
   },
   {
-    id: 1,
-    title: "Event 3",
+    id: 3,
+    title: "DEBBUGING MINDSET",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
+      "An engaging event for SRMIST students! Interactive session with seniors placed in companies like Capgemini, F5 networks, Amazon , Wipro  etc with  24lpa, 20lpa and so on.",
   },
 ];
 
