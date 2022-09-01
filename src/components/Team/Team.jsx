f0b541804bb5a7ae3ff653e41981fca61fa5dd01
Drish-xD <drish.xd@gmail.com>
2022-09-02 01:09:31 +0530

    added new team

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -7,7 +7,7 @@ let data = [
     id: 1,
     name: "Deepak Gupta",
     desc: "Chairperson",
-    img: "test.webp",
+    img: "Deepak.webp",
     link1: "https://www.linkedin.com/in/spycaptain/",
     link2: "https://www.github.com/",
     link3: "https://www.instagram.com/_spycaptain/",
@@ -16,7 +16,7 @@ let data = [
     id: 2,
     name: "Dhruv Pandey",
     desc: "Vice Chairperson",
-    img: "test.webp",
+    img: "Dhruv.webp",
     link1: "https://www.linkedin.com/in/dp2001",
     link2: "https://github.com/pandeydhruv2001",
     link3: "https://www.instagram.com/__the__neverending__footsteps_/",
@@ -25,7 +25,7 @@ let data = [
     id: 3,
     name: "Drish",
     desc: "Technical Head",
-    img: "test.webp",
+    img: "Drish.webp",
     link1: "https://www.linkedin.com/in/drish-xd/",
     link2: "https://github.com/Drish-xD",
     link3: "https://www.instagram.com/drish_xd/",
@@ -34,25 +34,25 @@ let data = [
     id: 4,
     name: "Aditya Prabhakar",
     desc: "Event Head",
-    img: "test.webp",
+    img: "Aditya.webp",
     link1: "https://www.linkedin.com/in/aditya-prabhakar-b9a8a11a6",
-    link2: "https://www.github.com/",
-    link3: "https://www.instagram.com/",
+    link2: "",
+    link3: "",
   },
   {
     id: 5,
     name: "Lahari Krishnam",
     desc: "Marketing Head",
-    img: "test.webp",
+    img: "Lahari.webp",
     link1: "https://www.linkedin.com/in/anushka-kanwar-8484241b6",
-    link2: "https://www.github.com/",
+    link2: "",
     link3: "https://www.instagram.com/anushkakanwar15/",
   },
   {
     id: 6,
     name: "Srishti Dhingra",
     desc: "Social Media Head",
-    img: "test.webp",
+    img: "Srishti.webp",
     link1: "https://www.linkedin.com/in/srishti-dhingra-8797851b9",
     link2: "",
     link3: "https://www.instagram.com/srishti2631/",
@@ -61,7 +61,7 @@ let data = [
     id: 7,
     name: "Pushpak Kumawat",
     desc: "Design/Branding Head",
-    img: "test.webp",
+    img: "Pushpak.webp",
     link1: "https://www.linkedin.com/in/pushpak-kumawat-b4bb921ba/",
     link2: "https://github.com/1pushpak1",
     link3: "https://www.instagram.com/_pushpak_k_/",
@@ -70,7 +70,7 @@ let data = [
     id: 8,
     name: "Samriddha Basu",
     desc: "P.R. Head",
-    img: "test.webp",
+    img: "Samriddha.webp",
     link1: "http://www.linkedin.com/in/samriddha-basu-cloud",
     link2: "https://github.com/samriddha-basu-cloud",
     link3: "https://www.instagram.com/unsheathed.dmg/",
@@ -91,50 +91,61 @@ let cardgrid = data.map((data) => {
   );
 });
 
-let othermembers = [
-  {
-    team: "Technical Team",
-    member: ["Aastha", "Drish", "Tanish Khare"],
-  },
-  {
-    team: "Event Management Team",
-    member: ["Aryan Gaur", "Chithjyot Kaur", "Shivansh Kaul"],
-  },
-  {
-    team: "Design and Branding Team",
-    member: ["Dhruv Pandey", "Pushpak Kumawat"],
-  },
-  {
-    team: "Marketing Team",
-    member: ["Kshitiz Priyam", "Manas Kumar", "Shrishti Dhingra"],
-  },
-  {
-    team: "Public Relation and Outreach Team",
-    member: ["Sohem Mondal", "Abhijeet Lakhera"],
-  },
-  {
-    team: "Social Media Team",
-    member: ["Lahri Krishnam", "Palak Dwivedi", "Shrishti Srivastava"],
-  },
-];
+// let othermembers = [
+//   {
+//     team: "Technical Team",
+//     member: ["Aastha", "Drish", "Tanish Khare"],
+//   },
+//   {
+//     team: "Event Management Team",
+//     member: ["Aryan Gaur", "Chithjyot Kaur", "Shivansh Kaul"],
+//   },
+//   {
+//     team: "Design and Branding Team",
+//     member: ["Dhruv Pandey", "Pushpak Kumawat"],
+//   },
+//   {
+//     team: "Marketing Team",
+//     member: ["Kshitiz Priyam", "Manas Kumar", "Shrishti Dhingra"],
+//   },
+//   {
+//     team: "Public Relation and Outreach Team",
+//     member: ["Sohem Mondal", "Abhijeet Lakhera"],
+//   },
+//   {
+//     team: "Social Media Team",
+//     member: ["Lahri Krishnam", "Palak Dwivedi", "Shrishti Srivastava"],
+//   },
+// ];
 
-let otherTeamMembers = othermembers.map((member) => {
-  let members = member.member.map((name) => <p key={name}>{name}</p>);
-  return (
-    <div key={member.team} className="member-grid">
-      <h3>{member.team}</h3>
-      {members}
-    </div>
-  );
-});
+// let otherTeamMembers = othermembers.map((member) => {
+//   let members = member.member.map((name) => <p key={name}>{name}</p>);
+//   return (
+//     <div key={member.team} className="member-grid">
+//       <h3>{member.team}</h3>
+//       {members}
+//     </div>
+//   );
+// });
+
+const faculty = (
+  <Card
+    title="Bharti Vidhury"
+    desc="Assistent Professor Department of CSE"
+    img="Faculty.webp"
+  />
+);
+
 const Team = () => {
   return (
     <section className="team" id="Team">
       <div>
         <h1 className="section-title">Team Leads</h1>
         <div className="team-container">{cardgrid}</div>
-        <h1 className="section-title">Other Team Members</h1>
-        <div className="team-container">{otherTeamMembers}</div>
+        {/* <h1 className="section-title">Other Team Members</h1>
+        <div className="team-container">{otherTeamMembers}</div> */}
+        <h1 className="section-title">Faculty Coordinator</h1>
+        <div className="team-container">{faculty}</div>
       </div>
     </section>
   );
