e7ee2f956cf12d74e687e6ca18b149080aa5c928
Drish <79963160+Drish-xD@users.noreply.github.com>
2022-10-04 00:00:43 +0530

    Dev (#5)
    * updated form
    
    * update images
    
    * linkedin link change
    
    * form closed
    
    * updated readme

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loader.css";
+
+const Loader = () => {
+  return (
+    <>
+      <div className="loader-container">
+        <div className="loader"></div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
--- src/pages/Recruitment.jsx
+++ src/pages/Recruitment.jsx
@@ -6,10 +6,11 @@ import Announcement from "react-announcement";
 import { withCookies, useCookies } from "react-cookie";
 import Logo from "../images/bell.png";
 import { supabase } from "../../lib/supabase";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 const Recruitment = () => {
   const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [cookies, removeCookie] = useCookies();
 
   useEffect(() => {
@@ -17,17 +18,28 @@ const Recruitment = () => {
   }, [removeCookie]);
 
   const submitData = async (data, e) => {
+    setLoading(true);
     await supabase
       .from("Recruitment")
-      .insert({ ...data, "resume": data.name + "-" + Date.now() })
-      .then(await supabase.storage.from("recruitment").upload(`resume/${data.name}-${Date.now()}.pdf`, data.resume[0]))
+      .insert({ ...data, resume: data.name + "-" + Date.now() })
+      .then(
+        await supabase.storage
+          .from("recruitment")
+          .upload(`resume/${data.name}-${Date.now()}.pdf`, data.resume[0])
+      )
       .then(() => {
         e.target.reset();
+        setLoading(false);
         setSubmitted(true);
-        emailjs.send(process.env.GATSBY_EMAIL_ID, process.env.GATSBY_TEMPLATE_ID, {
-          "name": data.name,
-          "email": data.email
-        }, process.env.GATSBY_EMAIL_KEY);
+        emailjs.send(
+          process.env.GATSBY_EMAIL_ID,
+          process.env.GATSBY_TEMPLATE_ID,
+          {
+            name: data.name,
+            email: data.email,
+          },
+          process.env.GATSBY_EMAIL_KEY
+        );
         setTimeout(() => {
           setSubmitted(false);
         }, 20000);
@@ -42,6 +54,11 @@ const Recruitment = () => {
       <Seo title="Recruitment For Core Members 2022" />
       <Layout>
         <h1 className="section-title">Recruitment Form</h1>
+        <RegistrationForm
+          submitData={submitData}
+          submitted={submitted}
+          loading={loading}
+        />
         {submitted && (
           <Announcement
             title="Submitted"
@@ -51,7 +68,6 @@ const Recruitment = () => {
             secondsBeforeBannerShows={0}
           />
         )}
-        <RegistrationForm submitData={submitData} submitted={submitted} />
       </Layout>
     </>
   );
