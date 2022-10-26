d0a7251ae42f720f43585906310b4c5f7ef417ac
Drish <79963160+Drish-xD@users.noreply.github.com>
2022-10-26 18:36:51 +0530

    Added Api in lib (#12)
    * added Api in lib

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -2,29 +2,9 @@ import React from "react";
 import Layout from "../components/Layout";
 import Event from "../components/Event/Event";
 import Seo from "../components/Seo";
+import { EventsApi } from "/lib/EventAPI";
 
-let events = [
-  {
-    id: 1,
-    title: "Resume Building Session",
-    description:
-      "This resume building workshop will help you build a polished resume that could land you an interview.🥳",
-  },
-  {
-    id: 2,
-    title: "KODATION",
-    description:
-      "An amazing technical Contest specially only for the SRMIST Delhi NCR students, conducted on it's very own trusted platform of GeeksforGeeks, this contest was divided into two groups.",
-  },
-  {
-    id: 3,
-    title: "DEBBUGING MINDSET",
-    description:
-      "An engaging event for SRMIST students! Interactive session with seniors placed in companies like Capgemini, F5 networks, Amazon , Wipro  etc with  24lpa, 20lpa and so on.",
-  },
-];
-
-let eventlist = events.map((data) => {
+let eventlist = EventsApi.map((data) => {
   return (
     <Event
       key={data.id}
