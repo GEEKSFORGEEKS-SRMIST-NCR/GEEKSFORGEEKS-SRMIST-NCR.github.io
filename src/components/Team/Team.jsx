e55a3888f225f04e80281c8a17df5c87c1028bc4
TanmayNewatia <98212774+TanmayNewatia@users.noreply.github.com>
2022-10-30 01:15:42 +0530

    Tanmay n (#15)
    * Added Api in lib (#12)
    
    * added Api in lib
    
    * Popup Added
    
    Co-authored-by: Drish <79963160+Drish-xD@users.noreply.github.com>

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -2,33 +2,42 @@ import React from "react";
 import Card from "./card";
 import { data } from "/lib/MemberApi";
 import "./Team.css";
-
-let leadCard = data.leadList.map((lead) => {
-  return (
-    <Card
-      key={lead.id}
-      title={lead.name}
-      desc={lead.desc}
-      img={lead.img}
-      link1={lead.link1}
-      link2={lead.link2}
-      link3={lead.link3}
-    />
-  );
-});
-
-let facultyCard = data.facultyList.map((faculty) => {
-  return (
-    <Card
-      key={faculty.name}
-      title={faculty.name}
-      desc={faculty.desc}
-      img={faculty.img}
-    />
-  );
-});
+import { useState } from "react";
 
 const Team = () => {
+  const [popup, setPopup] = useState(false);  
+  let leadCard = data.leadList.map((lead) => {
+    return (
+      <Card
+        key={lead.id}
+        title={lead.name}
+        desc={lead.desc}
+        img={lead.img}
+        link1={lead.link1}
+        link2={lead.link2}
+        link3={lead.link3}
+        ds={lead.ds}
+        popup={popup}
+        setPopup={setPopup}
+        logo={lead.logo}
+      />
+    );
+  });
+
+  let facultyCard = data.facultyList.map((faculty) => {
+    return (
+      <Card
+        key={faculty.name}
+        title={faculty.name}
+        desc={faculty.desc}
+        img={faculty.img}
+        ds={faculty.ds}
+        popup={popup}
+        setPopup={setPopup}
+      />
+    );
+  });
+
   return (
     <section className="team" id="Team">
       <div>
--- src/components/Team/card.jsx
+++ src/components/Team/card.jsx
@@ -1,46 +1,110 @@
 import React from "react";
 import "./Team.css";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiOutlineClose,
+} from "react-icons/ai";
+import "./Popup.css";
 
-const card = ({ title, desc, img, link1, link2, link3 }) => {
+const card = ({
+  title,
+  desc,
+  img,
+  link1,
+  link2,
+  link3,
+  ds,
+  popup,
+  setPopup,
+  logo,
+}) => {
+  console.log(logo);
+
+  const toggleModal = () => {
+    setPopup(!popup);
+  };
+
+  if (popup) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: `url(${require(`../../images/Team/${img}`).default})`,
-      }}
-    >
-      <div className="card-border">
-        <div className="card-info">
-          <h1>{title}</h1>
-          <p>{desc}</p>
-        </div>
+    <>
+      <div
+        onClick={toggleModal}
+        className="card"
+        style={{
+          backgroundImage: `url(${
+            require(`../../images/Team/${img}`).default
+          })`,
+        }}
+      >
+        <div className="card-border">
+          <div className="card-info">
+            <h1>{title}</h1>
+            <p>{desc}</p>
+          </div>
 
-        <div className="social-links">
-          {!link1 ? (
-            <></>
-          ) : (
-            <a href={link1} target="_blank" rel="noreferrer" title="LinkedIn">
-              <AiFillLinkedin />
-            </a>
-          )}
-          {!link2 ? (
-            <></>
-          ) : (
-            <a href={link2} target="_blank" rel="noreferrer" title="Github">
-              <AiFillGithub />
-            </a>
-          )}
-          {!link3 ? (
-            <></>
-          ) : (
-            <a href={link3} target="_blank" rel="noreferrer" title="Instagram">
-              <AiFillInstagram />
-            </a>
-          )}
+          <div className="social-links">
+            {!link1 ? (
+              <></>
+            ) : (
+              <a href={link1} target="_blank" rel="noreferrer" title="LinkedIn">
+                <AiFillLinkedin />
+              </a>
+            )}
+            {!link2 ? (
+              <></>
+            ) : (
+              <a href={link2} target="_blank" rel="noreferrer" title="Github">
+                <AiFillGithub />
+              </a>
+            )}
+            {!link3 ? (
+              <></>
+            ) : (
+              <a
+                href={link3}
+                target="_blank"
+                rel="noreferrer"
+                title="Instagram"
+              >
+                <AiFillInstagram />
+              </a>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      {popup && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <div className="modal-head">
+              <span><h2>{desc}</h2>
+              <p>Slogan</p></span>
+              <div>
+                {logo && (
+                  <img
+                    className="modallogo"
+                    src={require(`../../images/teamlogo/${logo}`).default}
+                  />
+                )}
+                <div className="close-modal" onClick={toggleModal}>
+                  <AiOutlineClose />
+                </div>
+              </div>
+            </div>
+            <div className="modal-body">
+              <img src={require(`../../images/Team/${img}`).default} />
+              <p>{ds}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
