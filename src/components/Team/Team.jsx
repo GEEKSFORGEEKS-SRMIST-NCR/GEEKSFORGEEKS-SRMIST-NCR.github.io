e9f7792e2124f6a800658db03ec6d566a154bd87
Harsh Gupta <30728750+guptaharsh940@users.noreply.github.com>
2022-11-01 22:25:26 +0530

    Updated Photos & Content  (#20)
    * Added Image of 2 Faculty & Updated Description
    
    * Changed Description for Director & Dean Adm.
    
    * Updated Photos & Title
    
    Co-authored-by: Drish <79963160+Drish-xD@users.noreply.github.com>

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -15,7 +15,7 @@ const Team = () => {
   return (
     <section className="team" id="Team">
       <div>
-        <h2 className="section-title">Faculty</h2>
+        <h2 className="section-title">Our Patrons</h2>
         <div className="team-container">{facultyCard}</div>
         <h2 className="section-title">Team Leads</h2>
         <div className="team-container">{leadCard}</div>
