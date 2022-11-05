2411c62e3b7d5e5803412849d478666b076bc653
TanmayNewatia <tr8497@srmist.edu.in>
2022-11-05 22:13:28 +0530

    Upcoming Event added

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -22,8 +22,16 @@ const Events = () => {
       <Layout>
         <h1 className="section-title">Events</h1>
         <h2 className="eventh2">Upcoming Events</h2>
-        <div className="events-container"><Event title={"Tech Aagaz"} desc="dassdkjbsjfbfjbdasfbd hfbhadfvdfhbdfajds hfhdfiadhfidfjdahf" img={7} /></div>
-        <h2 className="eventh2"><br></br>Past Events</h2>
+        <div className="events-container">
+          <Event
+            title={"Tech Aagaz"}
+            desc="GeeksforGeeks SRM IST Delhi-NCR Student Chapter is inviting students for an inductive session on completing 1-year. There will be an 'Inductive session' with teachers and seniors to briefly introduce GeeksforGeeks and its 1-year journey until now."
+            img={7}
+          />
+        </div>
+        <h2 className="eventh2">
+          <br></br>Past Events
+        </h2>
         <div className="events-container">{eventlist}</div>
         <h2 className="upcoming">MORE EVENTS COMING SOON !!!</h2>
       </Layout>
