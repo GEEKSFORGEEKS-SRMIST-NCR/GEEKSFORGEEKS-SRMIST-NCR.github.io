b60b6ccba005682dc3673fac4e18cbac7b0d7ea3
Drish <79963160+Drish-xD@users.noreply.github.com>
2022-11-06 10:10:48 +0530

    Update Confetti.jsx

--- src/components/Confetti/Confetti.jsx
+++ src/components/Confetti/Confetti.jsx
@@ -4,34 +4,25 @@ import ReactConfetti from 'react-confetti';
 
 const Confetti = () => {
     const [windowSize, setWindowSize] = useState({
-
     width: undefined,
-
     height: undefined,
-
   });
 
   useEffect(() => {
+    if (typeof window === 'undefined') return;
 
     function handleResize() {
-
       setWindowSize({
-
         width: window.innerWidth,
-
         height: window.innerHeight,
-
       });
-
     }
 
     window.addEventListener("resize", handleResize);
-
     handleResize();
-
     return () => window.removeEventListener("resize", handleResize);
+  }, [windowSize]);
 
-  }, []);
     return (
         <>
             <ReactConfetti
