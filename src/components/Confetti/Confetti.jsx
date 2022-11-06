ab898bcf22be2bbf01e07c8cd1250544498c6a2f
Tanish2002 <tanishkhare@gmail.com>
2022-11-06 20:35:33 +0530

    fix confetti issues

--- src/components/Confetti/Confetti.jsx
+++ src/components/Confetti/Confetti.jsx
@@ -1,15 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import ReactConfetti from 'react-confetti';
-import useWindowSize from 'react-use/lib/useWindowSize'
+import React, { useEffect, useState } from "react";
+import ReactConfetti from "react-confetti";
 
 const Confetti = () => {
-  const { width, height } = useWindowSize()
+  const size = useWindowSize();
+
   return (
-    <ReactConfetti
-      width={width}
-      height={height}
-      tweenDuration={5}
-    />
-  )
+    <>
+      <ReactConfetti
+        width={size.width}
+        height={size.height}
+        tweenDuration={5}
+      />
+    </>
+  );
+};
+
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
 }
+
 export default Confetti;
