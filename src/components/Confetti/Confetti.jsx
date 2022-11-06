18b3031ba264d9e21d667d7b151cb76bf91abf4e
Tanish2002 <tanishkhare@gmail.com>
2022-11-06 19:57:30 +0530

    fix confetti issues

--- src/components/Confetti/Confetti.jsx
+++ src/components/Confetti/Confetti.jsx
@@ -1,29 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import ReactConfetti from 'react-confetti';
+import useWindowSize from 'react-use/lib/useWindowSize'
 
 const Confetti = () => {
-    const [windowDimension, setDimension] = useState({width: window.innerWidth, height: window.innerHeight});
-    
-    const detectSize = () => {
-        setDimension({width: window.innerWidth, height: window.innerHeight});
-    }
-
-    useEffect(()=>{
-        window.addEventListener('resize',detectSize);
-        return ()=>{
-            window.removeEventListener('resize',detectSize);
-        }
-    },[windowDimension]);
-
-    return (
-        <>
-            <ReactConfetti
-                width={windowDimension.width}
-                height={windowDimension.height}
-                tweenDuration={5}
-            />    
-        </>
-    );
-};
-
+  const { width, height } = useWindowSize()
+  return (
+    <ReactConfetti
+      width={width}
+      height={height}
+      tweenDuration={5}
+    />
+  )
+}
 export default Confetti;
