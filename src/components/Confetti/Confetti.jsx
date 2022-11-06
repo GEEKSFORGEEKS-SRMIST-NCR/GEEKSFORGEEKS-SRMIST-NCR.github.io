f80a0d229dd07ed3c44ef6be4b9844b9509a7e4a
Tanish2002 <tanishkhare@gmail.com>
2022-11-06 23:53:29 +0530

    revert confetti

--- src/components/Confetti/Confetti.jsx
+++ src/components/Confetti/Confetti.jsx
@@ -1,41 +1,16 @@
-import React, { useEffect, useState } from "react";
-import ReactConfetti from "react-confetti";
+import React from 'react';
+import ReactConfetti from 'react-confetti';
+import useWindowSize from 'react-use/lib/useWindowSize'
 
 const Confetti = () => {
-  const size = useWindowSize();
-
+  const { width, height } = useWindowSize()
   return (
-    <>
-      <ReactConfetti
-        width={size.width}
-        height={size.height}
-        tweenDuration={5}
-      />
-    </>
-  );
-};
-
-// Hook
-function useWindowSize() {
-  const isSSR = typeof window !== "undefined";
-  const [windowSize, setWindowSize] = useState({
-    width: isSSR ? undefined : window.innerWidth,
-    height: isSSR ? undefined : window.innerHeight,
-  });
-
-  function changeWindowSize() {
-    setWindowSize({ width: window.innerWidth, height: window.innerHeight });
-  }
-
-  useEffect(() => {
-    window.addEventListener("resize", changeWindowSize);
-
-    return () => {
-      window.removeEventListener("resize", changeWindowSize);
-    };
-  }, []);
-
-  return windowSize;
+    <ReactConfetti
+      width={width}
+      height={height}
+      tweenDuration={5}
+    />
+  )
 }
 
 export default Confetti;
