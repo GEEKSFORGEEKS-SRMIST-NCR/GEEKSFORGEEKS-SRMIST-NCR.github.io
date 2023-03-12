2fa226e0d6d5f991aed347e4ad2ca33067753dbe
TanmayNewatia <98212774+TanmayNewatia@users.noreply.github.com>
2023-03-12 10:22:02 +0530

    Events Updated (#47)

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -24,11 +24,6 @@ const Events = () => {
         <h1 className="section-title">Events</h1>
         <h2 className="eventh2">Upcoming Events</h2>
         <div className="events-container">
-        <Event2
-            title={"Talk with Love Babbar"}
-            desc=""
-            img={9}
-          />
           <Event2
             title={"Tech-Darshan'23"}
             desc=""
