f02497f32ce241fec38d9bf25fae7f117ceb14a9
Drish-xD <drish.xd@gmailcom>
2023-03-30 18:29:21 +0530

    added navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar/Navbar";
 
 export default function Layout({ children }) {
   return (
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import Link from "next/link";
+import { useState } from "react";
+import styles from "styles/Home/Navbar.module.css";
+import { Logo } from "../Logo/Logo";
+import ThemeToogle from "./ThemeToogle";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  // Toogle Menu for small devices
+  const openMenu = () => {
+    if (window.innerWidth <= 1024) {
+      setOpen(!open);
+    }
+  };
+
+  return (
+    <header className={styles.header}>
+      {/* GFG Logo */}
+      <Link href="/" className={styles.logo}>
+        <Logo />
+      </Link>
+      {/* Menu Toogle Btn */}
+      <div
+        className={styles.munu_btn}
+        onClick={openMenu}
+        onKeyDown={openMenu}
+        role="button"
+        tabIndex="0"
+      >
+        <span
+          style={{
+            transform: open ? "rotate(45deg) translate(2px, -1px)" : "none",
+          }}
+        />
+        <span
+          style={{
+            opacity: open ? "0" : "1",
+          }}
+        />
+        <span
+          style={{
+            transform: open ? "rotate(-45deg)" : "none",
+          }}
+        />
+      </div>
+
+      {/* Navbar List */}
+      <ul className={open ? `${styles.ul + " " + styles.open}` : styles.ul}>
+        <li>
+          <Link href="/" onClick={openMenu}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/Events" onClick={openMenu}>
+            Events
+          </Link>
+        </li>
+        <li>
+          <Link href="/#Team" onClick={openMenu}>
+            Team
+          </Link>
+        </li>
+        <li>
+          <Link href="/Gallery" onClick={openMenu}>
+            Gallery
+          </Link>
+        </li>
+        <li>
+          <Link href="/#Contact" onClick={openMenu}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      <ThemeToogle />
+    </header>
+  );
+};
+
+export default Navbar;
