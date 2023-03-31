880b300f2f6c332fb8ede7de3ed738d92f1b1055
Drish-xD <drish.xd@gmailcom>
2023-03-31 20:53:11 +0530

    added optimized fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
+import { Roboto_Mono, Space_Mono } from "next/font/google";
 import "styles/globals.css";
 import { Layout } from "../components";
+
+const Roboto = Roboto_Mono({ subsets: ["latin"] });
+const Space = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function App({ Component }) {
   return (
@@ -7,6 +11,24 @@ export default function App({ Component }) {
       <Layout>
         <Component />
       </Layout>
+
+      {/* Added Optimized Fonts */}
+      <style jsx global>{`
+        h1,
+        h2,
+        h3,
+        h4,
+        h5 {
+          font-family: ${Space.style.fontFamily};
+        }
+
+        p,
+        q,
+        li {
+          font-family: ${Roboto.style.fontFamily};
+          word-spacing: 0.2rem;
+        }
+      `}</style>
     </>
   );
 }
